d13cd05ae60f555e484d732fff90e8a56bb3e029
dudgns0612 <dudgns0612@naver.com>
2021-07-13 22:47:04 +0900

    greedy_5545 테스트 완료

--- yhkim/index.js
+++ yhkim/index.js
@@ -2,32 +2,76 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-let inputCnt = 0;
-const inputData = [];
+let lineCnt = 0;
+let toppingCnt = 0;
 
-rl.on('line', function(line) {
-  console.log(line);
-  if (inputCnt === 0) {
-      inputCnt = Number(line);
+const calorie = {
+  douth: 0,
+  toppings: [],
+};
+
+const price = {
+  dough: 0,
+  topping: 0,
+};
+
+rl.on('line', function (line) {
+  if (lineCnt === 0) {
+    toppingCnt = Number(line);
+  } else if (lineCnt === 1) {
+    const prices = line.split(' ');
+    price.dough = Number(prices[0]);
+    price.topping = Number(prices[1]);
+  } else if (lineCnt === 2) {
+    calorie.dough = Number(line);
   } else {
-    if (inputData.length !== inputCnt) {
-      inputData.push(line);
+    calorie.toppings.push(Number(line));
 
-      if (inputCnt === inputData.length) {
-        solution(inputData);
-        rl.close();
-      }
+    if (toppingCnt === calorie.toppings.length) {
+      solution(calorie, price);
+      rl.close();
     }
   }
 
-}).on("close", function() {
+  lineCnt++;
+}).on('close', function () {
   process.exit();
 });
 
-function solution(data) {
-    console.log(data);
-}
+const solution = (calorie, price) => {
+  calorie.toppings = getDescSortArr(calorie.toppings);
 
+  let result = getCaloriePriceRatio(calorie.dough, [], price.dough);
+
+  for (let i = 0; i < calorie.toppings.length; i++) {
+    const pizzaPrice = price.dough + price.topping * (i + 1);
+    const calorieRatio = getCaloriePriceRatio(
+      calorie.dough,
+      calorie.toppings.slice(0, i + 1),
+      pizzaPrice
+    );
+
+    result = result > calorieRatio ? result : calorieRatio;
+  }
+
+  console.log(result);
+  return result;
+};
+
+const getCaloriePriceRatio = (douth, toppings, price) => {
+  let calorie = douth;
+  for (let i = 0; i < toppings.length; i++) {
+    calorie += toppings[i];
+  }
+
+  return Math.floor(calorie / price);
+};
+
+const getDescSortArr = (array) => {
+  const result = [...array];
+
+  return result.sort((a, b) => b - a);
+};
--- yhkim/prevTest/greedy_5545.js
+++ yhkim/prevTest/greedy_5545.js
@@ -1,2 +1,77 @@
-// 진행 된 완료 테스트 파일
-// 알고리즘 종류_테스트번호로 파일생성
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let lineCnt = 0;
+let toppingCnt = 0;
+
+const calorie = {
+  douth: 0,
+  toppings: [],
+};
+
+const price = {
+  dough: 0,
+  topping: 0,
+};
+
+rl.on('line', function (line) {
+  if (lineCnt === 0) {
+    toppingCnt = Number(line);
+  } else if (lineCnt === 1) {
+    const prices = line.split(' ');
+    price.dough = Number(prices[0]);
+    price.topping = Number(prices[1]);
+  } else if (lineCnt === 2) {
+    calorie.dough = Number(line);
+  } else {
+    calorie.toppings.push(Number(line));
+
+    if (toppingCnt === calorie.toppings.length) {
+      solution(calorie, price);
+      rl.close();
+    }
+  }
+
+  lineCnt++;
+}).on('close', function () {
+  process.exit();
+});
+
+const solution = (calorie, price) => {
+  calorie.toppings = getDescSortArr(calorie.toppings);
+
+  let result = getCaloriePriceRatio(calorie.dough, [], price.dough);
+
+  for (let i = 0; i < calorie.toppings.length; i++) {
+    const pizzaPrice = price.dough + price.topping * (i + 1);
+    const calorieRatio = getCaloriePriceRatio(
+      calorie.dough,
+      calorie.toppings.slice(0, i + 1),
+      pizzaPrice
+    );
+
+    result = result > calorieRatio ? result : calorieRatio;
+  }
+
+  console.log(result);
+  return result;
+};
+
+const getCaloriePriceRatio = (douth, toppings, price) => {
+  let calorie = douth;
+  for (let i = 0; i < toppings.length; i++) {
+    calorie += toppings[i];
+  }
+
+  return Math.floor(calorie / price);
+};
+
+const getDescSortArr = (array) => {
+  const result = [...array];
+
+  return result.sort((a, b) => b - a);
+};
