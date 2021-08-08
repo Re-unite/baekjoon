810810ab41488bc481b8ce77dc2f814c0f009ee1
dudgns0612 <dudgns0612@naver.com>
2021-08-08 16:13:17 +0900

    greedy_19939 테스트 완료

--- yhkim/index.js
+++ yhkim/index.js
@@ -4,54 +4,25 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-// 상근이가 가진 카드
-let haveCards = [];
-let haveCardSize = 0;
-// 결과를 구해야하는 카드
-let resultCards = [];
-let resultCardSize = 0;
-
 rl.on('line', function (line) {
-  if (haveCardSize === 0) {
-    haveCardSize = Number(line);
-  } else if (haveCards.length === 0) {
-    haveCards = line.split(' ');
-  } else if (resultCardSize === 0) {
-    resultCardSize = Number(line);
-  } else {
-    resultCards = line.split(' ');
-    if (resultCards.length === resultCardSize) {
-      solution(haveCards, resultCards);
-      rl.close();
-    }
-  }
+  const [ball, basket] = line.split(' ');
+  solution(Number(ball), Number(basket));
 }).on('close', function () {
   process.exit();
 });
 
-function solution(haveCards, resultCards) {
-  // 가진 카드 갯수 셋 생성
-  const haveCardsData = getHaveCountData(haveCards);
-
-  const resultCounts = [];
-  // 각 카드 숫자 카운트
-  for (const resultCard of resultCards) {
-    resultCounts.push(haveCardsData[resultCard] ? haveCardsData[resultCard] : 0);
+function solution(ball, basket) {
+  let result = 0;
+  const minCount = ((basket + 1) * basket) / 2;
+  // 최소 공의 갯수가 안될경우
+  if (minCount > ball) {
+    result = -1;
+    // 남은 공의 갯수가 바구니와 수와 일치 할 경우
+  } else if ((minCount - ball) % basket === 0) {
+    result = basket - 1;
+  } else {
+    result = basket;
   }
 
-  console.log(resultCounts.join(' '));
+  console.log(result);
 }
-
-// 카드 셋 생성
-const getHaveCountData = (haveCards) => {
-  const haveCardsData = {};
-  for (const haveCard of haveCards) {
-    if (haveCardsData[haveCard]) {
-      haveCardsData[haveCard] += 1;
-    } else {
-      haveCardsData[haveCard] = 1;
-    }
-  }
-
-  return haveCardsData;
-};
