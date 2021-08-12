c577426790f05dc547a16ea59a1809ff9ae7c43d
QyujinLee <gyujin89@gmail.com>
2021-08-12 22:32:02 +0900

    테스트 문제 목록 추가

--- gjlee/index.js
+++ gjlee/index.js
@@ -11,35 +11,4 @@ rl.on('line', function (line) {
   process.exit();
 });
 
-const solution = (num) => {
-  let result = -1;
-
-  if (num == 3) {
-    result = 1;
-  } else if (num >= 5 && num <= 5000) {
-    const len = num / 5;
-    let target = -1;
-
-    for (let idx = 0; idx <= len; idx++) {
-      let n;
-
-      if (idx === 0) {
-        n = 0;
-      } else {
-        n = 5 * idx;
-      }
-
-      const diffNum = num - n;
-
-      if (diffNum % 3 === 0) {
-        target = idx + diffNum / 3;
-      }
-
-      if (target != -1 && (result == -1 || (result != -1 && target < result))) {
-        result = target;
-      }
-    }
-  }
-
-  console.log(result);
-};
+const solution = (num) => {};
