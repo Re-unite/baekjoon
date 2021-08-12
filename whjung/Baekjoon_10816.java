06040f897262b15c39319386e3871cf6c033b5b8
jwhlll <jwhlll@hanmail.net>
2021-08-12 16:41:53 +0900

    못풀었네...

--- whjung/Baekjoon_10816.java
+++ whjung/Baekjoon_10816.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Baekjoon_10816 {
+import java.util.*;
+
+public class Baekjoon_10816 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        Map<Integer, Integer> result = new HashMap();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<size; i++) {
+            int number = sc.nextInt();
+            result.put(number, result.getOrDefault(number, 0) + 1);
+        }
+        int checkSize = sc.nextInt();
+        for(int i=0; i<checkSize; i++) {
+            int number = sc.nextInt();
+            sb.append(result.getOrDefault(number, 0) + " ");
+        }
+        System.out.println(sb.toString());
+    }
 }
--- whjung/Baekjoon_19939.java
+++ whjung/Baekjoon_19939.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Baekjoon_19939 {
+import java.util.Scanner;
+
+public class Baekjoon_19939 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ball = sc.nextInt();
+        int basket = sc.nextInt();
+        int result = -1;
+        int plusAll = basket * (basket+1) / 2;
+
+        if(ball >= plusAll) {
+            if((ball-plusAll) % basket == 0) {
+                result = basket - 1;
+            } else {
+                result = basket;
+            }
+        }
+
+        System.out.println(result);
+    }
 }
