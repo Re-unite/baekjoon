0346fb849a46b11d8a9147019587a3d573229dfe
dudgns0612 <dudgns0612@naver.com>
2021-08-15 22:08:11 +0900

    hash_1764 테스트 완료

--- yhkim/index.js
+++ yhkim/index.js
@@ -4,25 +4,43 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+let hearPersonCount = 0;
+let sayPersonCount = 0;
+const hearPersons = [];
+const sayPersons = [];
+
 rl.on('line', function (line) {
-  const [ball, basket] = line.split(' ');
-  solution(Number(ball), Number(basket));
+  if (hearPersonCount === 0 || sayPersonCount === 0) {
+    const hearSayCount = line.split(' ');
+    hearPersonCount = Number(hearSayCount[0]);
+    sayPersonCount = Number(hearSayCount[1]);
+  } else if (hearPersons.length !== hearPersonCount) {
+    hearPersons.push(line);
+  } else {
+    sayPersons.push(line);
+    if (sayPersons.length === sayPersonCount) {
+      solution(hearPersons, sayPersons);
+    }
+  }
 }).on('close', function () {
   process.exit();
 });
 
-function solution(ball, basket) {
-  let result = 0;
-  const minCount = ((basket + 1) * basket) / 2;
-  // 최소 공의 갯수가 안될경우
-  if (minCount > ball) {
-    result = -1;
-    // 남은 공의 갯수가 바구니와 수와 일치 할 경우
-  } else if ((minCount - ball) % basket === 0) {
-    result = basket - 1;
-  } else {
-    result = basket;
+function solution(hearPersons, sayPersons) {
+  // 듣도 보도 못한 사람 정렬 배열 생성
+  const hearSaytPersons = [...hearPersons, ...sayPersons];
+  hearSaytPersons.sort();
+  const hearSaySamePersons = [];
+
+  // 정렬 기준 같은 단어 배열 생성
+  for (let i = 0; hearSaytPersons.length - 1 > i; i++) {
+    if (hearSaytPersons[i] === hearSaytPersons[i + 1]) {
+      hearSaySamePersons.push(hearSaytPersons[i]);
+    }
   }
 
-  console.log(result);
+  console.log(hearSaySamePersons.length);
+  hearSaySamePersons.forEach((hearSayPerson) => {
+    console.log(hearSayPerson);
+  });
 }
