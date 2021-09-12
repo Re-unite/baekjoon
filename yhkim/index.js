4ba719be7e10657cd452cf8e073710b7831d5c3e
dudgns0612 <dudgns0612@naver.com>
2021-09-12 20:16:57 +0900

    greedy_1343 테스트 완료

--- yhkim/index.js
+++ yhkim/index.js
@@ -4,43 +4,19 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let hearPersonCount = 0;
-let sayPersonCount = 0;
-const hearPersons = [];
-const sayPersons = [];
-
 rl.on('line', function (line) {
-  if (hearPersonCount === 0 || sayPersonCount === 0) {
-    const hearSayCount = line.split(' ');
-    hearPersonCount = Number(hearSayCount[0]);
-    sayPersonCount = Number(hearSayCount[1]);
-  } else if (hearPersons.length !== hearPersonCount) {
-    hearPersons.push(line);
-  } else {
-    sayPersons.push(line);
-    if (sayPersons.length === sayPersonCount) {
-      solution(hearPersons, sayPersons);
-    }
-  }
+  solution(line);
 }).on('close', function () {
   process.exit();
 });
 
-function solution(hearPersons, sayPersons) {
-  // 듣도 보도 못한 사람 정렬 배열 생성
-  const hearSaytPersons = [...hearPersons, ...sayPersons];
-  hearSaytPersons.sort();
-  const hearSaySamePersons = [];
+function solution(polyomino) {
+  let result = polyomino.replace(/XXXX/g, 'AAAA');
+  result = result.replace(/XX/g, 'BB');
 
-  // 정렬 기준 같은 단어 배열 생성
-  for (let i = 0; hearSaytPersons.length - 1 > i; i++) {
-    if (hearSaytPersons[i] === hearSaytPersons[i + 1]) {
-      hearSaySamePersons.push(hearSaytPersons[i]);
-    }
+  if (result.includes('X')) {
+    result = -1;
   }
 
-  console.log(hearSaySamePersons.length);
-  hearSaySamePersons.forEach((hearSayPerson) => {
-    console.log(hearSayPerson);
-  });
+  console.log(result);
 }
